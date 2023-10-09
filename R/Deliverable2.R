e504a0350299da9e42857a62ae06eac18692ba35
sbas2405 <juans.gonzalezc@ecci.edu.co>
2023-10-08 21:22:43 -0500

    .

--- R/Deliverable2.R
+++ R/Deliverable2.R
@@ -1,18 +1,11 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+library(tidyverse)
+library(caret)
+library(dplyr)
+library(class)
+library(gmodels)
+folder<-dirname(rstudioapi::getSourceEditorContext()$path)
+parentFolder <-dirname(folder)
+data <- read.csv(paste0(parentFolder,"/DATASET/diabetes_012.csv"))
 
-hello <- function() {
-  print("Hello, world!")
-}
+data$Diabetes_012 <- ifelse(data$Diabetes_012 == 0, 0, 1)
+
