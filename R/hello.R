5ae087d1c58d657b3357fddb2eceb524a73eec5b
sbas2405 <juans.gonzalezc@ecci.edu.co>
2023-10-08 21:14:07 -0500

    ..

--- R/hello.R
+++ R/hello.R
@@ -3,3 +3,9 @@ library(caret)
 library(MASS)
 library(glmnet)
 library(boot)
+folder<-dirname(rstudioapi::getSourceEditorContext()$path)
+parentFolder <-dirname(folder)
+data <- read.csv(paste0(parentFolder,"/DATASET/diabetes_012.csv"))
+
+data$Diabetes_012 <- ifelse(data$Diabetes_012 == 0, 0, 1)
+
